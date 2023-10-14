1074fd03882247d5cf5c1e7e915fa9788dc76473
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-13 22:03:19 -0300

    Lesson 10 - Starting

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A
-git commit -m "Lesson 09 - Completed"
+git commit -m "Lesson 10 - Starting"
 git push
 git status
 
-# continue in https://www.theodinproject.com/lessons/foundations-problem-solving
+# continue in https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events
